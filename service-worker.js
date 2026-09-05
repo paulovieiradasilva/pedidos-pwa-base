804ca12f8011ebfc392606b03a61a3c8a7fa356f
Paulo <vieiraweb.tecnologia@gmail.com>
2026-09-05 13:31:52 -0300

    feat: add PWA manifest and offline service worker

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'pedidos-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './js/db.js',
+  './js/customers.js',
+  './js/products.js',
+  './js/orders.js',
+  './js/printer.js',
+  './js/app.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
